15ffba2c40f72e2c72e3158a659a7e8d720ab00e
ryan <drjonesy@users.noreply.github.com>
2016-07-11 11:22:37 -0700

    Add files via upload

---
+++ AvailableCourses.js
@@ -0,0 +1,25 @@
+/*
+	------------------------------------------------------------------------------------------
+	    These are all String values: meaning they must be between single or double quotes
+	------------------------------------------------------------------------------------------
+	The three values are Title, Description, and starting quiz link
+	Add a new using the following code:
+	
+		addCourse(
+			'title',
+			'description',
+			'link'
+		);
+
+	NOTE: the link should look similiar to:  course/chapter1.html or com262/chapter1.html
+
+*/
+
+
+
+addCourse(
+	'Com262 - Android Development',
+	'Learn basic Android development, gui and xml design. Use Intents to run other application with your custom app.',
+	'com262/chapter1.html'
+	);
+
